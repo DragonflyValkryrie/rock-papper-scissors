b4e8b74d4893b5457976d91bc612b7ccc4d76a24
DragonflyValkyrie <148598429+DragonflyValkryrie@users.noreply.github.com>
2023-11-08 12:38:06 -0500

    Refactored and style the game
    Refactored and style the game
    Refactored the code for scoring and DOM manipulation
    Added CSS styles and buttons to throw a move
    Added CSS styles for the scoring display
    Added new utility functions for game logic
    Added images

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-function getComputerChoice(){
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
+const winLimit = 5;
+
+const container = document.querySelector('#resultContainer');
+const divResult = document.createElement("div");
+const winResult = document.createElement("div");
+
+const rockButton = document.querySelector(".rock-button");
+const paperButton = document.querySelector(".paper-button");
+const scissorButton = document.querySelector(".scissor-button");
+const newGameButton = document.querySelector(".new-game-button");
+
+function getComputerChoice() {
     let computerGuess = Math.floor(Math.random() * 3);
 
-    if (computerGuess == 0){
+    if (computerGuess === 0) {
         return "rock";
-    } else if (computerGuess == 1) {
+    } else if (computerGuess === 1) {
         return "paper";
     } else {
         return "scissor";
     }
 }
 
-function playRound(playerSelection, computerSelection){
-
+function playRound(playerSelection, computerSelection) {
     const outcomes = {
-        rock: {rock: "tie", paper: "computer wins", scissor: "player wins"},
-        paper: {rock: "player wins", paper: "tie", scissor: "computer wins"},
-        scissor: {rock: "computer wins", paper: "player wins", scissor: "tie"}
+        rock: { rock: "tie", paper: "computer wins", scissor: "player wins" },
+        paper: { rock: "player wins", paper: "tie", scissor: "computer wins" },
+        scissor: { rock: "computer wins", paper: "player wins", scissor: "tie" }
     }
     return outcomes[playerSelection][computerSelection];
 }
@@ -25,39 +38,86 @@ function isValidThrow(playerSelection) {
     return validThrows.includes(playerSelection);
 }
 
-let playerScore = 0;
-let computerScore = 0;
-let tieScore = 0;
+function resetScores() {
+    playerScore = 0;
+    computerScore = 0;
+    tieScore = 0;
 
-function gameScore(result){
+    enableButtons();
 
-    if (result === "player wins"){
-        playerScore += 1
-    } else if (result === 'computer wins'){
-        computerScore +=1;
-    } else
-        tieScore += 1;
-
-    console.log(`Player score is: ${playerScore} Computer score is: ${computerScore} and you have tied ${tieScore} times`);
+    divResult.textContent = "";
+    winResult.textContent = "";
 }
-function game(){
-    let playerSelection = prompt("What do you throw?").toLowerCase();
 
-    while (!isValidThrow(playerSelection)) {
-        alert("Valid throws only. Please try again.");
-        playerSelection = prompt("What do you throw?").toLowerCase();
+function gameScore(result) {
+    if (result === "player wins") {
+        playerScore += 1;
+    } else if (result === 'computer wins') {
+        computerScore += 1;
+    } else {
+        tieScore += 1;
     }
 
+    console.log(`Player score is: ${playerScore} Computer score is: ${computerScore} and you have tied ${tieScore} times`);
+    divResult.textContent = `Player score is: ${playerScore} Computer score is: ${computerScore} and you have tied ${tieScore} times`;
+    resultContainer.appendChild(divResult);
+    winCondition(playerScore, computerScore);
+}
+
+function winCondition(playerScore, computerScore) {
+    if (playerScore === winLimit || computerScore === winLimit) {
+        if (playerScore === winLimit) {
+            winResult.textContent = "Player wins the game!";
+            console.log("Player wins the game!");
+        } else {
+            winResult.textContent = "Computer wins the game!";
+            console.log("Computer wins the game!");
+        }
+        resultContainer.appendChild(winResult);
+        disableButtons();
+        newGame();
+    }
+}
+
+function newGame() {
+    newGameButton.style.display = "block";
+    newGameButton.addEventListener("click", () => {
+        resetScores();
+        newGameButton.style.display = "none";
+    });
+}
+
+function disableButtons() {
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorButton.disabled = true;
+}
+
+function enableButtons() {
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorButton.disabled = false;
+}
+
+let playerSelection = 0;
+
+rockButton.addEventListener("click", () => {
+    game("rock");
+});
+
+paperButton.addEventListener("click", () => {
+    game("paper");
+});
+
+scissorButton.addEventListener("click", () => {
+    game("scissor");
+});
+
+function game(playerSelection) {
     let computerSelection = getComputerChoice();
     let result = playRound(playerSelection, computerSelection);
     console.log(`Player chose ${playerSelection}`);
     console.log(`Computer chose ${computerSelection}`);
-
     gameScore(result);
-
     console.log(result);
-}
-
-for (let i = 0; i < 4; i++){
-    game();
 }
